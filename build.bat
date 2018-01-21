8bf72df21625ca5de6d9d4cb68cefda5aee35710
Ian Hills <v@bitvox.me>
2018-01-21 02:18:34 -0500

    small build fix

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 SET project_dir=%~dp0
 SET unity_bin="C:\Program Files\Unity\Editor\Unity.exe"
 
-rm -r "%project_dir%build"
+rmdir -r "%project_dir%build"
 %unity_bin% -nographics -quit -batchmode -projectPath "%project_dir%" -logFile -executeMethod Build.BuildWebGl || exit /b
 %unity_bin% -nographics -quit -batchmode -projectPath "%project_dir%" -logFile -executeMethod Build.BuildWindows || exit /b
 %unity_bin% -nographics -quit -batchmode -projectPath "%project_dir%" -logFile -executeMethod Build.BuildLinux || exit /b
